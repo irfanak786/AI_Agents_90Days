ccecbec952bc47648984d16ba0cf0b49deaaeed1
irfanak786 <arfanalikhan8@gmail.com>
2025-02-18 21:13:38 +0300

    Day 11: Created and showed multiple environments

---
+++ Day11.py
@@ -0,0 +1,12 @@
+# Creating and Showing the World (Environment) for AI Agent
+trash_bin = ["plastic", "paper", "glass", "food"]
+print("trash_bin",trash_bin)
+
+environment = ["water","road","school","bus"]
+print("environment",environment)
+down_town = ["shopping mall","parks","kisok", "university"]
+print("key_points",down_town)
+house_hold= ["bed","chair","sofa","iron","water tub"]
+print("house_hold_items: ", house_hold)
+personal_belongings = ["watch","mobile phone","car","wallet","jacket"]
+print("personal_belongings are: ",personal_belongings)
